d71dbaaeca4b55c0c036196e799e26137510fee3
danielmachado86 <danielmcis@gmail.com>
2021-09-23 01:47:19 +0000

    Modified save commit

--- truffle.js
+++ truffle.js
@@ -5,7 +5,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider')
 module.exports = {
   networks: {
     development: {
-      host: '127.0.0.1',
+      host: '172.18.0.1',
       port: 8545,
       network_id: '*',
     },
